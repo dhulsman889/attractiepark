9ff4b8eec5d6c11eb91fba6187563b2fbb417852
Damian <100926905+DamianHulsman@users.noreply.github.com>
2025-10-05 12:15:45 +0200

    First commit

--- start_code.py
+++ start_code.py
@@ -2,62 +2,77 @@
 from pathlib import Path
 import json
 import pprint
-from database_wrapper import Database
+import mysql.connector
+import mysql
+
+# from database_wrapper import Database
 
 
 # initialisatie
 
 # parameters voor connectie met de database
-db = Database(host="localhost", gebruiker="user", wachtwoord="password", database="attractiepark")
 
 
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="admin",
+  password="root",
+  database="attractiepark"
+)
+
+db = mydb.cursor()
 # main
 
 # Haal de eigenschappen op van een personeelslid
 # altijd verbinding openen om query's uit te voeren
-db.connect()
 
 # pas deze query aan om het juiste personeelslid te selecteren
 select_query = "SELECT * FROM personeelslid WHERE id = 1"
-personeelslid = db.execute_query(select_query)
+personeelslid = db.execute(select_query)
 
+myresult = db.fetchone()
+
+if myresult:
+    pprint.pp(myresult)
+else:
+    print("Geen personeelslid gevonden met het opgegeven ID.")
 # altijd verbinding sluiten met de database als je klaar bent
-db.close()
+# db.close()
 
-pprint.pp(personeelslid) # print de resultaten van de query op een overzichtelijke manier
-print(personeelslid[0]['naam']) # voorbeeld van hoe je bij een eigenschap komt
+# pprint.pp(personeelslid) # print de resultaten van de query op een overzichtelijke manier
+# print(personeelslid[0]['naam'])
 
 
 
 # Haal alle onderhoudstaken op
 # altijd verbinding openen om query's uit te voeren
-db.connect()
+# db.connect()
 
 # pas deze query aan en voeg queries toe om de juiste onderhoudstaken op te halen
-select_query = "SELECT * FROM onderhoudstaak"
-onderhoudstaken = db.execute_query(select_query)
+# select_query = "SELECT * FROM onderhoudstaak"
+# onderhoudstaken = db.execute(select_query)
 
 # altijd verbinding sluiten met de database als je klaar bent
-db.close()
+# db.close()
 
-#pprint.pp(onderhoudstaken) # print de resultaten van de query op een overzichtelijke manier
+# pprint.pp(onderhoudstaken) # print de resultaten van de query op een overzichtelijke manier
 
 
 
 # verzamel alle benodigde gegevens in een dictionary
-dagtakenlijst = {
-    "personeelsgegevens" : {
-        "naam": personeelslid[0]['naam'] # voorbeeld van hoe je bij een eigenschap komt
-        # STAP 1: vul aan met andere benodigde eigenschappen
-    },
-    "weergegevens" : {
-        # STAP 4: vul aan met weergegevens
-    }, 
-    "dagtaken": [] # STAP 2: hier komt een lijst met alle dagtaken
-    ,
-    "totale_duur": 0 # STAP 3: aanpassen naar daadwerkelijke totale duur
-}
+# dagtakenlijst = {
+#     "personeelsgegevens" : {
+#         "naam": personeelslid[0]['naam'] # voorbeeld van hoe je bij een eigenschap komt
+#         # STAP 1: vul aan met andere benodigde eigenschappen
+#     },
+#     "weergegevens" : {
+#         # STAP 4: vul aan met weergegevens
+#     }, 
+#     "dagtaken": [] # STAP 2: hier komt een lijst met alle dagtaken
+#     ,
+#     "totale_duur": 0 # STAP 3: aanpassen naar daadwerkelijke totale duur
+# }
 
 # uiteindelijk schrijven we de dictionary weg naar een JSON-bestand, die kan worden ingelezen door de acceptatieomgeving
-with open('dagtakenlijst_personeelslid_x.json', 'w') as json_bestand_uitvoer:
-    json.dump(dagtakenlijst, json_bestand_uitvoer, indent=4)
+# with open('dagtakenlijst_personeelslid_x.json', 'w') as json_bestand_uitvoer:
+#     json.dump(dagtakenlijst, json_bestand_uitvoer, indent=4)
